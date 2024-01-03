e2c7e89894432aff5673290ebbf3952c8c58e82e
nowingcanfly <92837978+nowingcanfly@users.noreply.github.com>
2024-01-04 02:31:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ MD_HEAD = """**<p align="center">[Leeyom's Blog](https://blog.leeyom.top)</p>**
 **<p align="center">所谓月光如华，奢夜如拥，总归是想有个碎碎念的地方而已</p>**
 [![](https://raw.githubusercontent.com/superleeyom/blog/main/img/IMG_216.JPEG)](https://blog.leeyom.top)
 
-## 联系方式
+<!-- ## 联系方式
 - Twitter：[@super_leeyom](https://twitter.com/super_leeyom)
 - Telegram：[@super_leeyom](https://t.me/super_leeyom)
 - Email：[leeyomwang@163.com](mailto:leeyomwang@163.com)
 - Blog：[https://blog.leeyom.top](https://blog.leeyom.top)
-- RSS：[RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
+- RSS：[RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml) -->
 """
 
 BACKUP_DIR = "backup"
